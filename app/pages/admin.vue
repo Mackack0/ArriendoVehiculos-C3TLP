c41491234533c8e4786e0d18e3977bd9a077715f
MULTILOQUIUM <MULTILOQUIUM@users.noreply.github.com>
2026-06-30 19:18:06 -0400

    Avances microscopicos pq tailwind me anda molestando >:C

---
+++ app/pages/admin.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <!--Titulo y Botones-->
+        <h1 class="text-2xl font-bold text-white">Administrador</h1>
+        <div class="flex">
+            <button class="bg-blue-300 hover:bg-blue-600 text-white font-semibold py-2 px-4 m-1.5 rounded">Añadir</button>
+            <button class="bg-blue-300 hover:bg-blue-600 text-white font-semibold py-2 px-4 m-1.5 rounded">Listar</button>
+            <button class="bg-blue-300 hover:bg-blue-600 text-white font-semibold py-2 px-4 m-1.5 rounded">Quitar</button>
+        </div>
+    </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/ui', 'nuxt-auth-utils'],
+  modules: ['@nuxt/ui', 'nuxt-auth-utils', '@nuxtjs/tailwindcss'],
   css: ['~/assets/css/main.css'],
   compatibilityDate: '2026-06-29',
   devtools: { enabled: true },
